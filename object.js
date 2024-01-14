cba416e58af8df4d9163a9786829b8641b5d36af
vishu9334 <144935874+vishu9334@users.noreply.github.com>
2024-01-14 11:31:01 +0000

    json talk

---
+++ object.js
@@ -0,0 +1,47 @@
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+// const course ={
+//     coursename: "js in hindi",
+//     price:"999",
+//     courseInstructor:"hitesh"
+// }
+
+// //course.courseInstructor
+
+// const {courseInstructor} = course
+// const {courseInstructor:instructor} = course
+// console.log(courseInstructor);
+// console.log(instructor);
+// -------------------------------------------------------------------------------------------
+
+// object DeStructure
+// const navbar = ({company}) => {
+
+// }
+
+// navbar (company = "hitesh")
+
+// ---------------------------------------------------------------------------------------
+
+// API
+
+//  this is api formate 
+
+// {
+//    " name": "vishal",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+//  also this is api formate 
+[
+    {},
+    {},
+    {}
+]
+// ---------------------------------------------------------------------------------------
